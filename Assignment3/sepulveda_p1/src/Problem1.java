38e34308cf345dae7fe4206d4a2f87b2ecb6ebed
Cris Sepulveda <csepulveda7@knights.ucf.edu>
2019-10-19 01:18:31 -0400

    Assignment 3 init

---
+++ Assignment3/sepulveda_p1/src/Problem1.java
@@ -0,0 +1,8 @@
+public class Problem1 {
+    public static void foo (){
+        // Do Something
+    }
+    public static void main (String[] args) {
+        foo();
+    }
+}
---
+++ Assignment3/sepulveda_p2/src/Problem2.java
@@ -0,0 +1,8 @@
+public class Problem2 {
+    public static void foo (){
+        // Do Something
+    }
+    public static void main (String[] args) {
+        foo();
+    }
+}
